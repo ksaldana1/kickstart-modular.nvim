05c67c736cee3701e19113dd906f735ef37e44e0
Kevin Saldana <ksaldana1@gmail.com>
2024-08-04 11:48:12 -0400

    Static opts for flit

--- lua/custom/plugins/flit.lua
+++ lua/custom/plugins/flit.lua
@@ -1,15 +1,13 @@
 return {
   'ggandor/flit.nvim',
   lazy = false,
-  config = function()
-    require('flit').setup {
-      keys = { f = 'f', F = 'F', t = 't', T = 'T' },
-      labeled_modes = 'nvo',
-      -- clever_repeat = true,
-      multiline = true,
-      opts = {},
-    }
-  end,
+  opts = {
+    keys = { f = 'f', F = 'F', t = 't', T = 'T' },
+    labeled_modes = 'nvo',
+    -- clever_repeat = true,
+    multiline = true,
+    opts = {},
+  },
   dependencies = {
     'ggandor/leap.nvim',
   },
