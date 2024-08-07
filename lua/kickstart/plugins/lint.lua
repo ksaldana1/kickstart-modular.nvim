b135efcbf9332a83b50771c9cc427b6f4784539d
Kevin Saldana <ksaldana1@gmail.com>
2024-08-07 15:53:13 -0400

    add ts linters

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -5,9 +5,11 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
+      lint.linters_by_ft = {}
+      lint.linters_by_ft['markdown'] = { 'markdownlint' }
+      lint.linters_by_ft['typescript'] = { 'eslint_d' }
+      lint.linters_by_ft['javascript'] = { 'eslint_d' }
+      lint.linters_by_ft['typescriptreact'] = { 'eslint_d' }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
