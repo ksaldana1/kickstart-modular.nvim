8ef2e91130c39a8bf8868f3222a570ef8c65c6c6
Kevin Saldana <ksaldana1@gmail.com>
2024-08-07 16:39:40 -0400

    disable tsserver diagnostics on tsserver

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -178,7 +178,11 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        tsserver = {
+          handlers = {
+            ['textDocument/publishDiagnostics'] = function() end,
+          },
+        },
         --
 
         lua_ls = {
