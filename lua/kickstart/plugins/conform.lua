12467f0a4aba3c7e9b484138dd00e99d08d3b5b1
Kevin Saldana <ksaldana1@gmail.com>
2025-05-14 16:27:32 +0700

    prefer biome

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -31,9 +31,9 @@ return {
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-         javascript = { "prettierd", "prettier", stop_after_first = true },
-         typescript = { "prettierd", "prettier", stop_after_first = true },
-         typescriptreact = { "prettierd", "prettier", stop_after_first = true }
+         javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+         typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
+         typescriptreact = { "biome", "prettierd", "prettier", stop_after_first = true }
       },
     },
   },
