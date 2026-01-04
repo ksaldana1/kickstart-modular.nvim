8ad8fe1801bdd4b8f0003941ef8400492286bdad
Kevin Saldaña <ksaldana1@gmail.com>
2026-01-04 10:20:24 +0900

    get rid of markdownlint

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,6 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {}
-      lint.linters_by_ft['markdown'] = { 'markdownlint' }
       -- lint.linters_by_ft['typescript'] = { 'eslint_d' }
       -- lint.linters_by_ft['javascript'] = { 'eslint_d' }
       -- lint.linters_by_ft['typescriptreact'] = { 'eslint_d' }
@@ -14,7 +13,6 @@ return {
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
       -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
       --
       -- However, note that this will enable a set of default linters,
       -- which will cause errors unless these tools are available:
@@ -24,7 +22,6 @@ return {
       --   inko = { "inko" },
       --   janet = { "janet" },
       --   json = { "jsonlint" },
-      --   markdown = { "vale" },
       --   rst = { "vale" },
       --   ruby = { "ruby" },
       --   terraform = { "tflint" },
@@ -37,7 +34,6 @@ return {
       -- lint.linters_by_ft['inko'] = nil
       -- lint.linters_by_ft['janet'] = nil
       -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
       -- lint.linters_by_ft['rst'] = nil
       -- lint.linters_by_ft['ruby'] = nil
       -- lint.linters_by_ft['terraform'] = nil
